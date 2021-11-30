c09cc5d0b1430aaaa86aa3f068e1d595f32ac663
brunosas88 <bruno.sas@live.com>
2021-11-29 21:33:12 -0300

    adicionando diretorios

---
+++ Front/Mod_4_Projeto_Final/Scripts/index.js
@@ -0,0 +1,8 @@
+import { menuController } from "./utility-functions.js"
+import { selectCity } from "./listener-functions.js"
+
+function main () {    
+    selectCity()
+    menuController()
+}
+main()
---
+++ Front/Mod_4_Projeto_Final/Scripts/request-functions.js
@@ -0,0 +1,21 @@
+import { adjustWords } from "./utility-functions.js"
+import * as creationFunction from "./creation-functions.js"
+import { cleanDynamicDiv } from "./listener-functions.js"
+
+export async function getApartaments (selection) {    
+    try {
+        const result = adjustWords (selection)
+            
+        const bruteData = await fetch(`https://private-9e061d-piweb.apiary-mock.com/venda?state=${result.state}&city=${result.city}`)
+        const data = await bruteData.json()
+        const dynamicDiv = document.querySelector('.dynamicDiv')       
+        dynamicDiv.innerHTML=''
+        dynamicDiv.append(creationFunction.createProductDivInfo({total: data.search.totalCount, address: data.search.result.listings[0].listing.address}))
+        data.search.result.listings.forEach(creationFunction.createProductDivView)
+        cleanDynamicDiv()
+    } catch (error) {
+        creationFunction.createErrorView()
+        console.log("Error in function getApartaments")
+    }
+}
+
