e5ec815f8e8e13952990d49a8eccce5be1d843d5
brunosas88 <bruno.sas@live.com>
2022-06-07 16:35:30 -0300

    estruturação de projeto

--- Mod_4_Projeto_Final/Scripts/index.js
+++
@@ -1,8 +0,0 @@
-import { menuController } from "./utility-functions.js"
-import { selectCity } from "./listener-functions.js"
-
-function main () {    
-    selectCity()
-    menuController()
-}
-main()
